929d5c166423ff57f379ac251d1eff59bc465013
sveta <sveta@sveta-pc>
2020-08-22 21:15:42 +0100

    DP

--- NetBeansProjects/aaa7/src/aaa7/Aaa7.java
+++ NetBeansProjects/aaa7/src/aaa7/Aaa7.java
@@ -5,147 +5,53 @@ import java.util.Scanner;
 
 
 public class Aaa7 {
-
     
-    public static void main(String[] args) {
+    static int M, C, K;
+    static int[][]price=new int[25][25];
+    static int[][]memo= new int[210][25];
+    
+    static int shop(int money_left, int garment_id){
+        
+        if (money_left <0)return -1;
+        if (garment_id == C) return M- money_left;
+        if (memo[money_left][garment_id] != -1)return memo[money_left][garment_id];
+        int max_value=-10;
+        for (int model=1;model<=price[garment_id][0];model++){
+            max_value=Math.max(max_value, shop(money_left-price[garment_id][model], garment_id +1));
+            
+        }return memo[money_left][garment_id]=max_value;
+    }
+    
+
+    public static void main(String[]args){
+        
         Scanner scn=new Scanner(System.in);
         
-        while(true){
-            int a=scn.nextInt();
-            int b=scn.nextInt();
-            
-            if (a==0 && b==0)break;
-            
-            int[]head=new int[a];
-            int[]knight=new int[b];
-            long pay=0;
-            
-            for (int i=0;i<a;i++){
-                head[i]=scn.nextInt();
-            }
-            
-            for (int i=0;i<b;i++){
-                knight[i]=scn.nextInt();
-            }
-            if (a>b){
-                doomed();
-                continue;
-            }
-            boolean cont=true;
-            int knightInd=0;
-            head=mergesort(head);
-            knight=mergesort(knight);
-            
-            for (int h=0;h<a;h++){
-                int hDiam=head[h];
-                
-                while (knight[knightInd]<hDiam){
-                    knightInd++;
-                    if (knightInd==b){
-                        cont=false;
-                        break;
-                    }
+        int score, TC,i,j;
+        
+        TC=scn.nextInt();
+        while(TC--!=0){
+            M=scn.nextInt();
+            C=scn.nextInt();
+            for (i=0;i<C;i++){
+                price[i][0]=scn.nextInt();
+                for (j=1;j<=price[i][0];j++){
+                    price[i][j]=scn.nextInt();
                 }
-                pay+=knight[knightInd];
-                knightInd++;
-                if (!cont)break;
             }
-            if(!cont){
-                doomed();
-                
-            }
-            else{
-                System.out.println(pay);
-            }
-            
+        
+        initialize();
+        score = shop(M,0);
+        if(score <0)System.out.println("no solution");
+        else System.out.println(score);
         }
-        
-        
     }
-    static void doomed(){
-        String lose="Loowater is doomed!";
-        System.out.println(lose);
+    static void initialize(){
+        for (int i=0;i<210;i++){
+            for (int j=0;j<25;j++){
+                memo[i][j]=-1;
+            }
+        }
     }
-    static int[] mergesort(int[] arr ){
-        
-        int n=arr.length;
-        if (n==1)return arr;
-        
-        if (n==2){
             
-            int a=arr[0];
-            int b=arr[1];
-            if (a>b){
-                
-                arr[0]=b;
-                arr[1]=a;
-                
-            }
-            return arr;
-            
-        }
-        int len=n/2;
-        int[] a=new int[len];
-        int[] b=new int[n-len];
-        
-        for (int i=0;i<len;i++){
-            a[i]=arr[i];
-        }
-        for (int i=0;i<n-len;i++){
-            b[i]=arr[i+len];
-        }
-        
-        a=mergesort(a);
-        b=mergesort(b);
-        int[]res=new int[n];
-        
-        int ia=0;
-        int ib=0;
-        
-        for (int i=0;i<n;i++){
-            int na=a[ia];
-            int nb=b[ib];
-            
-            if (na>nb){
-                
-                res[i]=b[ib];
-                
-                ib++;
-                
-            }
-            else{
-                
-                res[i]=a[ia];
-                
-                ia++;
-            }
-            
-            if (ia==len){
-                for (int y=ib;y<n-len;y++){
-                    i++;
-                    res[i]=b[y];
-                    
-               }
-                break;
-            }
-            else if (ib==n-len){
-                for (int y=ia;y<len;y++){
-                    i++;
-                    res[i]=a[y];
-                    
-                }
-                break;
-            }
-        }
-        return res;
-    }
 }
-
-
-
-
-
-
-
-
-
