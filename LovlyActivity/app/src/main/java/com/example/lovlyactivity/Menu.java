4c7ba9dd55f9f57904bb22aea08bf3653c820cfe
sveta <sveta@sveta-pc>
2020-12-08 20:44:26 +0000

    Podavki

---
+++ LovlyActivity/app/src/main/java/com/example/lovlyactivity/Menu.java
@@ -0,0 +1,23 @@
+package com.example.lovlyactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void Play(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+
+    }
+}
